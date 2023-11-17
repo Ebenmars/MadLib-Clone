3df4038373f677333dcf84e0a32e74fd1b8468b8
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-11-17 17:26:29 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        String adjective;
+        String girlName;
+        String adjective2;
+        String occupation;
+        String place;
+        String clothing;
+        String hobby;
+        String adjective3;
+        String occupation2;
+        String mansName;
+        String boysName;
+        System.out.print("Enter an adjective: ");
+        adjective = scan.nextLine();
+        System.out.print("Enter a girl's name: ");
+        girlName = scan.nextLine();
+        System.out.print("Enter another adjective: ");
+        adjective2 = scan.nextLine();
+        System.out.print("Enter an occupation: ");
+        occupation = scan.nextLine();
+        System.out.print("Enter the name of a place: ");
+        place = scan.nextLine();
+        System.out.print("Enter the name of a piece of clothing: ");
+        clothing = scan.nextLine();
+        System.out.print("Enter the name of a hobby: ");
+        hobby = scan.nextLine();
+        System.out.print("Enter another adjective: ");
+        adjective3 = scan.nextLine();
+        System.out.print("Enter another occupation: ");
+        occupation2 = scan.nextLine();
+        System.out.print("Enter a boy's name: ");
+        boysName = scan.nextLine();
+        System.out.print("Enter a man's name: ");
+        mansName = scan.nextLine();
+
+
+        System.out.println("There once was a " + adjective + "girl named " + girlName + " who was a " + adjective2 + occupation
+                + "in the Kingdom of " + place + "She loved to wear " + clothing + " and to" + hobby + " . She wanted to marry the orange " +
+                occupation2 + " named " + boysName + " but her father, King " + mansName + " forbid her seeing him."
+                );
+
     }
 }
